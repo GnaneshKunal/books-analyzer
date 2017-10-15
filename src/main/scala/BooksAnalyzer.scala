2783affbf2e122dae61ec4ff2cfd3b2992adfe5a
gnanesh <gnaneshkunal@outlook.com>
2017-10-15 09:26:14 +0530

    use sparkSession instead of old sc

--- src/main/scala/BooksAnalyzer.scala
+++ src/main/scala/BooksAnalyzer.scala
@@ -1,11 +1,15 @@
 import org.apache.log4j._
 import org.apache.spark._
+import org.apache.spark.sql.SparkSession
+
+import scala.io.Codec
 
 object BooksAnalyzer extends App {
 
   Logger.getLogger("org").setLevel(Level.ERROR)
 
-  val sc = new SparkContext("local[*]", "Books Analyzer")
+  val spark = SparkSession.builder.appName("Books Analyzer").master("local[*]").getOrCreate()
+  val sc = spark.sparkContext
 
   var parallelize = sc.parallelize("Hello World".toList)
 
