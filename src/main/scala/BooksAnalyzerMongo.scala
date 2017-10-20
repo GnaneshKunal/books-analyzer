55572a9e8fa8af7e27ae20acba44ccbcfbea0bfd
gnanesh <gnaneshkunal@outlook.com>
2017-10-20 23:50:59 +0530

    basic mongodb spark example

---
+++ src/main/scala/BooksAnalyzerMongo.scala
@@ -0,0 +1,39 @@
+import org.apache.log4j._
+import org.apache.spark.sql._
+import org.apache.spark._
+import com.mongodb._
+import com.mongodb.spark.MongoSpark
+
+object BooksAnalyzerMongo extends App {
+
+  Logger.getLogger("org").setLevel(Level.ERROR)
+
+  val spark = SparkSession
+    .builder()
+    .master("local[*]")
+    .appName("Books Analyzer Mongo")
+    .config("spark.mongodb.input.uri", "mongodb://monster:monster@127.0.0.1:27017/book-miner.bookreviews")
+    .config("spark.mongodb.output.uri", "mongodb://monster:monster@127.0.0.1:27017/book-miner.bookreviews")
+    .getOrCreate()
+
+  val sc = spark.sparkContext
+
+  val rdd = MongoSpark.load(sc)
+
+  val filteredRdd = rdd.filter(_.getInteger("bookID") < 10)
+  println {
+    filteredRdd.first.toJson
+  }
+
+  println {
+    rdd.count
+  }
+
+//  val documents = sc.parallelize((1 to 10).map(i => Document.parse(s"{spark: $i")))
+
+//  documents.collect.foreach(println)
+
+  spark.stop()
+
+
+}
