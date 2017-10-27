ae24dd73cca339675224a9c433e92f9f43b30275
yolossn <nssvlr@gmail.com>
2017-10-27 21:53:04 +0530

    comment and clean  the code

--- src/main/scala/BooksAnalyzerMongo.scala
+++ src/main/scala/BooksAnalyzerMongo.scala
@@ -1,3 +1,4 @@
+//import statements
 import org.apache.log4j._
 import org.apache.spark.sql._
 import org.apache.spark._
@@ -5,9 +6,9 @@ import com.mongodb._
 import com.mongodb.spark.MongoSpark
 
 object BooksAnalyzerMongo extends App {
-
-  Logger.getLogger("org").setLevel(Level.ERROR)
-
+ //set log level to error
+  Logger.getLogger("org").setLevel(Level.ERROR)//prints error logs only
+  // initialization
   val spark = SparkSession
     .builder()
     .master("local[*]")
@@ -15,58 +16,58 @@ object BooksAnalyzerMongo extends App {
     .config("spark.mongodb.input.uri", "mongodb://monster:monster@127.0.0.1:27017/book-miner.bookreviews")
     .config("spark.mongodb.output.uri", "mongodb://monster:monster@127.0.0.1:27017/book-miner.bookreviews")
     .getOrCreate()
-
+  
   val sc = spark.sparkContext
-
+  //load dataset from mongo db as a rdd
   val rdd = MongoSpark.load(sc).persist()
-
+  // print 
   println {
     "TOP 10 AUTHORS"
   }
-
+ 
   val top10Authors =
-    rdd.map(x => (x.getString("author"), 1))
-    .reduceByKey(_ + _)
-    .sortBy(_._2, ascending = false)
-    .take(10)
+    rdd.map(x => (x.getString("author"), 1)) //use map fn to get author and create tuple(author,no of book=1)
+    .reduceByKey(_ + _) //create condensed tuple (author,total no of books) where key is author 
+    .sortBy(_._2, ascending = false) //descending sort by number of books
+    .take(10) // take top 10 tuples alone 
 
-  top10Authors.foreach(println)
+  top10Authors.foreach(println) //print the top 10 authors
 
-  val harryPotterBooks = rdd.filter(_.get("title").toString.contains("Harry Potter"))
-
+  val harryPotterBooks = rdd.filter(_.get("title").toString.contains("Harry Potter")) //filter the rdd to get rows where book titles contain harry potter 
+  //print the count of harrypotter books
   println {
     "HARRY POTTER BOOKS COUNT: " + harryPotterBooks.count()
   }
-
+  
   println {
     "BOOKS WITH MOST EDITIONS"
   }
 
   val booksWithMostEditions =
-    rdd.map(x => (x.get("title"), 1))
-      .reduceByKey(_ + _)
-      .sortBy(_._2, ascending = false)
+    rdd.map(x => (x.get("title"), 1)) // use map fn to get title and create tuple(title,Edition count=1)
+      .reduceByKey(_ + _) //create condensed tuple (title,edition count) where key is title
+      .sortBy(_._2, ascending = false) //descending sort edition count
 
-  booksWithMostEditions.take(5).foreach(println)
+  booksWithMostEditions.take(5).foreach(println) //print top 5 books with most number of editions
 
   println {
     "BOOKS WITH 5 STAR REVIEWER RATINGS"
   }
 
-  val booksWithFiveStarReviewerRatings = rdd.filter(_.get("reviewerRatings") == 5)
+  val booksWithFiveStarReviewerRatings = rdd.filter(_.get("reviewerRatings") == 5) // filter rdd to get rows where reviewer ratings is 5 
 
-  booksWithFiveStarReviewerRatings.take(5).foreach(x => println(x.get("title")))
+  booksWithFiveStarReviewerRatings.take(5).foreach(x => println(x.get("title"))) //print first 5 book titles with 5 star rating
 
   println{
     "BOOKS WITH 1 STAR REVIEWER RATINGS"
   }
 
   val booksWith1StarRatings = rdd
-    .filter(_.get("reviewerRatings") == 1)
+    .filter(_.get("reviewerRatings") == 1) //filter rdd to get rows where reviewer ratings is 1
 
   booksWith1StarRatings
-    .take(5) foreach(x => println {
-    x.get("title")
+    .take(5) foreach(x => println { 
+    x.get("title")                //print first 5 book titles with 1 star rating
   })
 
   println {
@@ -74,10 +75,10 @@ object BooksAnalyzerMongo extends App {
   }
 
   val booksWith0StarRatings = rdd
-    .filter(_.get("reviewerRatings") == 0)
+    .filter(_.get("reviewerRatings") == 0) //filter rdd to get rows where reviewer ratings is 0
 
   booksWith0StarRatings
-    .take(5) foreach(x => println {
+    .take(5) foreach(x => println { //print first 5 book titles with 0 star rating
     x.get("title")
   })
 
@@ -86,9 +87,9 @@ object BooksAnalyzerMongo extends App {
   }
 
   val booksStartsWithA = rdd
-    .filter(_.get("title").toString.charAt(0) == 'A')
+    .filter(_.get("title").toString.charAt(0) == 'A') //filter rdd to get rows where book title start with a
 
-  booksStartsWithA take 5 foreach(x => println {
+  booksStartsWithA take 5 foreach(x => println { 
     x.getString("title")
   })
 
